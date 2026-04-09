27c75c2ea42f9efadf373f1f5c5c1220bb813c83
Pete Yandell <pete@notahat.com>
2026-04-09 20:12:36 +1000

    Initial Commit

---
+++ Shunt/ShuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShuntApp.swift
+//  Shunt
+//
+//  Created by Peter Yandell on 9/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
